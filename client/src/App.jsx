0904ea429d4b2cea463b8e5d8bf29f39e0c88efd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-25 19:43:59 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HoemPage from "./pages/HoemPage";
+import Nav from "./components/Nav/Nav";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<HoemPage /> }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/HoemPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HoemPage = () => {
+  return (
+    <div className='bg-[url("https://wallpapercave.com/wp/ppd85lw.jpg")] h-[80vh] w-full p-16 bg-cover bg-center'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea cupiditate asperiores non, eveniet culpa ducimus dolores beatae corrupti totam quam corporis quibusdam est voluptate vitae dignissimos dolore ratione natus similique.
+    </div>
+  )
+}
+
+export default HoemPage
