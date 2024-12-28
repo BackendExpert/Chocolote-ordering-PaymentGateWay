c0e4e41952cee6330d8f92f5427d7c38c9c69985
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-28 12:47:53 +0530

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#BC9773] px-4 md:px-32 mt-8'>
-      <div className="md:grid grid-cols-3 gap-3 py-16">
+    <div className='bg-[#BC9773] px-4 md:px-32 mt-8 pt-16 pb-10'>
+      <div className="md:grid grid-cols-3 gap-3 ">
 
         <div className="text-white">
           <h1 className="text-2xl font-semibold mb-4">Chocolate Ordering..</h1>
@@ -12,6 +12,28 @@ const Footer = () => {
           </p>
         </div>
 
+        <div className="text-white">
+          <h1 className="text-2xl font-semibold mb-4">Links..</h1>
+          <p className="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor consectetur aut laborum obcaecati, numquam iusto consequatur odio alias natus itaque voluptate repellendus dolore exercitationem reiciendis accusantium magni excepturi quia recusandae.
+          </p>
+          
+        </div>
+
+        <div className="text-white">
+          <h1 className="text-2xl font-semibold mb-4">Links..</h1>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur natus quis harum ducimus culpa sit atque ea et, animi sapiente, qui, minima illum asperiores eius quos dolorem tempora recusandae maxime.
+          </p>          
+        </div>
+
+      </div>
+
+      <hr className='border border-white mt-16'/>
+      <div className="">
+        <center className='text-center text-white pt-2'>
+          &copy; Chocolate Ordering | All rights Reserved | 2024-2025 | Developed and Maintained by <a href="" target='_blnk'>JehanKandy</a>
+        </center>
       </div>
     </div>
   )
