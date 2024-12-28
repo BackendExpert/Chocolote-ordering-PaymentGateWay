9c836e825803570ffc03bbdb66123ae339ab15b1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-28 12:42:39 +0530

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-[#BC9773] px-4 md:px-32 mt-8'>
+      <div className="md:grid grid-cols-3 gap-3 py-16">
+
+        <div className="text-white">
+          <h1 className="text-2xl font-semibold mb-4">Chocolate Ordering..</h1>
+          <p className="">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores ad voluptas fugiat dolore modi itaque, aspernatur molestiae ea officia nostrum rem? Autem esse corporis maiores minus consequatur veritatis! Dolorum, illum!
+          </p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
