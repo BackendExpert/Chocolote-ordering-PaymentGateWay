a72f8847d863e27f91d1a8ce163730352a011c0e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-28 12:56:32 +0530

    Update SignUp.jsx

--- client/src/pages/AuthPages/SignUp.jsx
+++ client/src/pages/AuthPages/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SubmitBtn from '../../components/Buttons/SubmitBtn'
 
 const SignUp = () => {
   return (
@@ -12,6 +13,45 @@ const SignUp = () => {
                   <h1 className="text-xl font-semibold text-center text-[#BC9773]">
                     Create New Account
                   </h1>
+
+                  <div className="">
+                    <form onSubmit={headleLogin} method="post">
+                        <div className="my-8">
+                            <div className="flex my-2">
+                                <p className="p-3 border rounded-l-lg">
+                                  <FaUserAlt className='h-8 w-auto fill-gray-400'/>
+                                </p>
+
+                                <InputDefault 
+                                    Type={"email"}
+                                    onChange={handleInputChange}
+                                    placeholder={"Email Address"}
+                                    value={LoginData.email}
+                                    name={'email'}
+                                    required={true}
+                                />
+                            </div>  
+
+                            <div className="flex my-4">
+                                <p className="p-3 border rounded-l-lg">
+                                  <FaKey className='h-8 w-auto fill-gray-400'/>                                        
+                                </p>
+                                <InputDefault 
+                                    Type={"password"}
+                                    onChange={handleInputChange}
+                                    placeholder={"Password"}
+                                    value={LoginData.password}
+                                    name={'password'}
+                                    required={true}
+                                />                                        
+                            </div>  
+
+                            <div className="">
+                                <SubmitBtn btntype={'submit'} btnvalue={'Create Account'}/>
+                            </div>                                    
+                        </div>
+                    </form>
+                  </div>
                 </div>
             </div>
         </div>
