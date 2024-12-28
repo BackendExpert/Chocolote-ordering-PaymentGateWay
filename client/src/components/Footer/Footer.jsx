22a9c0d4db1051a67e3c32080d121fee3b8902db
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-28 18:11:30 +0530

    Update

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#BC9773] px-4 md:px-32 pt-8 pt-16 pb-10'>
+    <div className='bg-[#7B3F00] px-4 md:px-32 pt-8 pt-16 pb-10'>
       <div className="md:grid grid-cols-3 gap-3 ">
 
         <div className="text-white">
--- client/src/pages/HomePage/OurCollection.jsx
+++ client/src/pages/HomePage/OurCollection.jsx
@@ -9,41 +9,41 @@ const OurCollection = () => {
                 <div className="my-4">
                     <div className="md:flex">
                         <div className="relative h-64 md:my-0 my-4 rounded-md md:w-full md:mx-4 overflow-hidden">
-                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp4144882.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
+                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp2126571.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
                             </div>
                         </div>
 
                         <div className="relative h-64 md:my-0 my-4 rounded-md md:w-full md:mx-4 overflow-hidden">
-                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp4144882.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
+                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp2126580.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
                             </div>
                         </div>
 
                         <div className="relative h-64 md:my-0 my-4 rounded-md md:w-full md:mx-4 overflow-hidden">
-                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp4144882.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
+                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp3088163.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
                             </div>
                         </div>
                                                
                     </div>
                     <div className="md:flex my-4">
                         <div className="relative h-64 md:my-0 my-4 rounded-md md:w-1/2 md:mx-4 overflow-hidden">
-                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp4144882.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
+                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp3088166.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
                             </div>
                         </div>
 
                         <div className="relative h-64 md:my-0 my-4 rounded-md md:w-full md:mx-4 overflow-hidden">
-                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp4144882.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
+                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp2126439.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
                             </div>
                         </div>
 
                     </div>
                     <div className="md:flex my-4">
                         <div className="relative h-64 md:my-0 my-4 rounded-md md:w-full md:mx-4 overflow-hidden">
-                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp4144882.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
+                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp13705806.png')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
                             </div>
                         </div>
 
                         <div className="relative h-64 md:my-0 my-4 rounded-md md:w-full md:mx-4 overflow-hidden">
-                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp4144882.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
+                            <div className="absolute inset-0 bg-[url('https://wallpapercave.com/wp/wp9710268.jpg')] bg-cover bg-center transition-transform duration-500 hover:scale-110">
                             </div>
                         </div>
 
