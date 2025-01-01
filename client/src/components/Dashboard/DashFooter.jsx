3ffe078064e381f3334bb60c0e921b5c735e7dc9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-01 11:20:12 +0530

    Update

--- client/src/components/Dashboard/DashFooter.jsx
+++ client/src/components/Dashboard/DashFooter.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const DashFooter = () => {
+
   return (
     <div className='text-xs text-gray-500 mb-4'>
       <p className="">&copy; Chocolate Ordering System | 2024 - 2025 | Developed and Maintained by <a href="" target='_blnk'>JehanKandy</a></p>
+    
+      <div className="">
+
+      </div>
     </div>
   )
 }
--- client/src/components/TableData/AdminOrderStatus.jsx
+++ client/src/components/TableData/AdminOrderStatus.jsx
@@ -1,6 +1,89 @@
 import React from 'react'
 
 const AdminOrderStatus = () => {
+    const tablesameledata = [
+        {
+          id: 1,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Online Card',
+          status: 'Approve'      
+        },
+        {
+          id: 2,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Cash on Delivery',
+          status: 'Pending'       
+        },
+        {
+          id: 3,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Online Card',
+          status: 'Approve'        
+        },
+        {
+          id: 4,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Online Card',
+          status: 'Approve'         
+        },
+        {
+          id: 5,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Cash on Delivery',
+          status: 'Pending'       
+        },
+        {
+          id: 6,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Cash on Delivery',
+          status: 'Approve'        
+        },
+        {
+          id: 7,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Online Card',
+          status: 'Rejected'        
+        },
+        {
+          id: 8,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Online Card',
+          status: 'Pending'         
+        },
+        {
+          id: 9,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Cash on Delivery',
+          status: 'Rejected'        
+        },
+        {
+          id: 10,
+          orderid: '123ABC',
+          name: 'Kamal',
+          mobile: '+094 711785581',
+          paymentmethod: 'Online Card',
+          status: 'Approve'       
+        }
+        
+       ]
   return (
     <div className='my-4'>
         <div className="bg-white rounded shadow p-4">
