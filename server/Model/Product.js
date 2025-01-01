0f0887cc44490ec558ccc2cb394177a156540f4c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-01 11:51:23 +0530

    Update Product.js

--- server/Model/Product.js
+++ server/Model/Product.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-
-});
+    productName: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stockQuantity: { type: Number, required: true, default: 0 },
+    brand: { type: String },
+    imageUrl: { type: Number, required: true },
+    weight: { type: String, required: true },
+    status: { type: String, required: true, default: 'Out of Stock' },    
+},{timestamps: true });
 
 const Product = mongoose.model('Product', ProductSchema);
 
