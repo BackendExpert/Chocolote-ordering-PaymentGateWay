f00d013895ec6f304defb369a6d68a2df737ee85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-01 10:38:04 +0530

    UPdate

--- client/src/pages/Dashboard/DashHome.jsx
+++ client/src/pages/Dashboard/DashHome.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
-import DashImg from '../../assets/AdminDash.png'
+import { useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
+import AdminDashHome from '../../components/Dashboard/AdminDashHome'
+import UserDashHome from '../../components/Dashboard/UserDashHome'
 
 const DashHome = () => {
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
   return (
     <div>
       <div className="">
         <h1 className="text-xl uppercase font-semibold text-[#696cff]">Dashboard</h1>
       </div>
-      <div className="md:flex my-2">
-        <div className="w-full bg-white p-4 mr-2 shadow rounded">
-          <div className="md:flex">
-            <div className="w-full">sss</div>
-            <div className="w-full">
-            <img src={DashImg} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="w-full bg-white p-4 ml-2">
+      {
+        (() => {
+          if(RoleUser === "admin"){
+            return (
+              <AdminDashHome />
+            )
+          }
+          else if(RoleUser === "user"){
+            return (
+              <UserDashHome />
+            )
+          }
+        })()
+      }
 
-        </div>
-      </div>
     </div>
   )
 }
