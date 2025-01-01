085fdb5db67dc60f4fb50c671d8e8dbd168678e5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-01 11:33:11 +0530

    Update AdminOrderStatus.jsx

--- client/src/components/TableData/AdminOrderStatus.jsx
+++ client/src/components/TableData/AdminOrderStatus.jsx
@@ -88,7 +88,75 @@ const AdminOrderStatus = () => {
     <div className='my-4'>
         <div className="bg-white rounded shadow p-4">
             <h1 className="text-xl font-semibold text-[#696cff]">Order Status</h1>
+
+            <div className="w-full my-4">
+                <table className='w-full'>
+                    <thead className='h-12 bg-[#696cff] text-white'>
+                        <tr>
+                            <th>ID</th>
+                            <th>Order ID</th>
+                            <th>Name</th>
+                            <th>Mobile</th>
+                            <th>Payment Method</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            tablesameledata.map((item, index) => {
+                                return (
+                                    <tr className='h-12 bg-white border-b border-[#696cff]/20' key={index}>
+                                        <td className='pl-4 font-semibold'>{item.id}</td>
+                                        <td className='pl-4 font-semibold'>{item.orderid}</td>
+                                        <td className='pl-4'>{item.name}</td>
+                                        <td className='pl-4'>{item.mobile}</td>
+                                        <td className='pl-4'>
+                                            {
+                                                (() => {
+                                                    if(item.paymentmethod === "Online Card"){
+                                                        return (
+                                                            <div className="text-[#696cff] font-semibold">{item.paymentmethod}</div>
+                                                        )
+                                                    }
+                                                    else if(item.paymentmethod === "Cash on Delivery"){
+                                                        return (
+                                                            <div className="text-[#696cff]">{item.paymentmethod}</div>
+                                                        )
+                                                    }
+                                                })()
+                                            }
+                                        </td>
+                                        <td className='pl-4'>
+                                            {
+                                                (() => {
+                                                    if(item.status === "Approve"){
+                                                        return (
+                                                            <div className="bg-green-500 text-center text-white py-1 rounded-full font-semibold">{item.status}</div>
+                                                        )
+                                                    }
+                                                    else if(item.status === "Pending"){
+                                                        return(
+                                                            <div className="bg-yellow-500 text-center text-white py-1 rounded-full font-semibold">{item.status}</div>
+                                                        )
+                                                    }
+                                                    else if(item.status === "Rejected"){
+                                                        return(
+                                                            <div className="bg-red-500 text-center text-white py-1 rounded-full font-semibold">{item.status}</div>
+                                                        )
+                                                    }
+                                                })()
+                                            }
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
+
+
     </div>
   )
 }
