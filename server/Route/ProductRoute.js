f8716b57205925e79dff98433aa5a396209dd087
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-01 11:55:28 +0530

    Update ProductRoute.js

--- server/Route/ProductRoute.js
+++ server/Route/ProductRoute.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const ProductController = require('../Controller/ProductController');
 
 const router = express.Router();
+
+router.post('/createproduct', ProductController.prodcutCreate)
 
 module.exports = router;
