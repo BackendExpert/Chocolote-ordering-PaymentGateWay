e468b63310117c235722a544f4c45d5fadae9647
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-01 11:20:53 +0530

    Update DashFooter.jsx

--- client/src/components/Dashboard/DashFooter.jsx
+++ client/src/components/Dashboard/DashFooter.jsx
@@ -5,10 +5,6 @@ const DashFooter = () => {
   return (
     <div className='text-xs text-gray-500 mb-4'>
       <p className="">&copy; Chocolate Ordering System | 2024 - 2025 | Developed and Maintained by <a href="" target='_blnk'>JehanKandy</a></p>
-    
-      <div className="">
-
-      </div>
     </div>
   )
 }
