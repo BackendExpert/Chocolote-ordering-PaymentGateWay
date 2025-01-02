a516bacbddaa9ebb96d1153545378176ff8f731a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-02 13:18:39 +0530

    Update CreateProduct.jsx

--- client/src/pages/Product/CreateProduct.jsx
+++ client/src/pages/Product/CreateProduct.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import DashInput from '../../components/forms/DashInput'
 
 const CreateProduct = () => {
   return (
@@ -8,6 +9,7 @@ const CreateProduct = () => {
       <div className="bg-white p-4  shadow-md rounded">
         <p className="">Product Name: </p>
         <input type="text" name="" id="" className="bg-gray-200 w-full h-12 border border-[#696cff] rounded pl-2" placeholder='Product Name' required/>
+        <DashInput />
       </div>
     </div>
   )
