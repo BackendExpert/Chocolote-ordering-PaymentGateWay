a9b7311e7cf873563a1b67717eebafffca87e36f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-02 13:24:53 +0530

    Update CreateProduct.jsx

--- client/src/pages/Product/CreateProduct.jsx
+++ client/src/pages/Product/CreateProduct.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashInput from '../../components/forms/DashInput'
 
 const CreateProduct = () => {
+    const [productData, setProductData] = useState({
+      productName: '',
+      description: '',
+      price: '',
+      stockQuantity: '',
+      brand: '',
+      image: '',
+      weight: '',
+    })
+
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setProductData((prevData) => ({
+        ...prevData,
+        [name]: value
+      }));
+    };
   return (
     <div>
       <h1 className="font-semibold text-[#696cff] text-xl">Create Product</h1>
 
       <div className="bg-white p-4  shadow-md rounded">
-        <p className="">Product Name: </p>
-        <input type="text" name="" id="" className="bg-gray-200 w-full h-12 border border-[#696cff] rounded pl-2" placeholder='Product Name' required/>
-        <DashInput />
+        <div className="">
+          <form method="post">
+            <div className="">
+              <p className="mb-2 text-lg text-gray-500">Product Name : </p>
+              <DashInput 
+                Type={'text'}
+                name={'productName'}
+                value={productData.productName}
+                onChange={handleInputChange}
+                required={true}
+                placeholder={'Product Name'}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
