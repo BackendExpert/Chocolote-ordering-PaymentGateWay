774b1b4f2df1a771594b6de4ac81bf0695f8453f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-02 13:09:22 +0530

    Update

---
+++ client/src/components/forms/DashInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DashInput = () => {
+  return (
+    <input 
+        type="text" 
+        name="" 
+        id="" 
+        className="bg-gray-200 w-full h-12 border border-[#696cff] rounded pl-2" 
+        placeholder='Product Name' 
+        required
+    />
+  )
+}
+
+export default DashInput
--- client/src/pages/Product/CreateProduct.jsx
+++ client/src/pages/Product/CreateProduct.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const CreateProduct = () => {
   return (
-    <div>CreateProduct</div>
+    <div>
+      <h1 className="font-semibold text-[#696cff] text-xl">Create Product</h1>
+
+      <div className="bg-white p-4">
+        <p className="">Product Name: </p>
+        <input type="text" name="" id="" className="bg-gray-200 w-full h-12 border border-[#696cff] rounded pl-2" placeholder='Product Name' required/>
+      </div>
+    </div>
   )
 }
 
