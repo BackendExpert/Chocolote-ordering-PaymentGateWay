24127ad139fe037f1270438a1686bb0f2eed0db6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-06 19:49:21 +0530

    Update ManageProduct.jsx

--- client/src/pages/Product/ManageProduct.jsx
+++ client/src/pages/Product/ManageProduct.jsx
@@ -4,6 +4,10 @@ const ManageProduct = () => {
   return (
     <div>
       <h1 className="font-semibold text-[#696cff] text-xl">Manage Product</h1>
+
+      <div className="bg-white p-4 rounded shadow-md m4">
+        
+      </div>
     </div>
   )
 }
