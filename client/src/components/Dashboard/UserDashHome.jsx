4a9a1bfb5c837c8935fa2879dafb6da99c77648b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-12 21:15:58 +0530

    Update

--- client/src/components/Dashboard/UserDashHome.jsx
+++ client/src/components/Dashboard/UserDashHome.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import CountUp from 'react-countup'
+import { CiBoxList } from "react-icons/ci";
+import { IoMegaphoneSharp } from "react-icons/io5";
+import { LuListCheck } from "react-icons/lu";
+
 
 const UserDashHome = () => {
+    const userDataCard = [
+      {
+        id: 1,
+        name: 'My Orders',
+        value: <CountUp end={20} />,
+        icon: <CiBoxList />
+      },
+      {
+        id: 2,
+        name: 'Available Offers',
+        value: <CountUp end={20} />,
+        icon: <IoMegaphoneSharp />
+      },
+      {
+        id: 3,
+        name: 'Success Orders',
+        value: <CountUp end={20} />,
+        icon: <LuListCheck />
+      },
+      {
+        id: 4,
+        name: 'My Expenses',
+        value: <CountUp end={20} />,
+        icon: ''
+      },
+
+    ]
   return (
-    <div>UserDashHome</div>
+    <div>
+      
+    </div>
   )
 }
 
--- server/Model/User.js
+++ server/Model/User.js
@@ -5,7 +5,6 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['admin', 'user'], required: true, default: "user" },  
-    address: {type: String }
 }, {timestamps: true });
 
 const User = mongoose.model('User', UserSchema);
