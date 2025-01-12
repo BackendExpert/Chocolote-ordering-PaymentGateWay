f4b5406fa8cfb700a1f88590930fced8bb2a23b7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-12 21:31:05 +0530

    Update UserDashHome.jsx

--- client/src/components/Dashboard/UserDashHome.jsx
+++ client/src/components/Dashboard/UserDashHome.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import CountUp from 'react-countup'
 import { CiBoxList } from "react-icons/ci";
 import { IoMegaphoneSharp } from "react-icons/io5";
-import { LuListCheck } from "react-icons/lu";
+import { FaWallet } from "react-icons/fa";
+import { FaListCheck } from "react-icons/fa6";
 
 
 const UserDashHome = () => {
@@ -11,31 +12,51 @@ const UserDashHome = () => {
         id: 1,
         name: 'My Orders',
         value: <CountUp end={20} />,
-        icon: <CiBoxList />
+        icon: <CiBoxList className='h-7 w-auto fill-[#696cff]'/>
       },
       {
         id: 2,
         name: 'Available Offers',
         value: <CountUp end={20} />,
-        icon: <IoMegaphoneSharp />
+        icon: <IoMegaphoneSharp className='h-7 w-auto fill-[#696cff]'/>
       },
       {
         id: 3,
         name: 'Success Orders',
         value: <CountUp end={20} />,
-        icon: <LuListCheck />
+        icon: <FaListCheck className='h-7 w-auto fill-[#696cff]'/>
       },
       {
         id: 4,
         name: 'My Expenses',
         value: <CountUp end={20} />,
-        icon: ''
+        icon: <FaWallet className='h-7 w-auto fill-[#696cff]'/>
       },
 
     ]
   return (
     <div>
-      
+      <div className="md:grid grid-cols-2 gap-4">
+        <div className="md:grid grid-cols-2 gap-4 my-4">
+          {
+            userDataCard.map((data, index) => {
+              return (
+                <div className="bg-white shadow-md rounded-md p-4" key={index}>
+                  <div className="flex justify-between">
+                    <h1 className="text-[#696cff] uppercase font-semibold">{data.name}</h1>
+                    <div className="">
+                      {data.icon}
+                    </div>
+                  </div>
+                  <div className="">
+                    <h1 className="text-3xl my-4 text-[#696cff] pl-4">{data.value}</h1>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   )
 }
