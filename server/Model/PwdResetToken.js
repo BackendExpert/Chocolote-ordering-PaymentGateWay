85adc25eded7d06e4db33737384517c233e9f2d9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-17 20:26:47 +0530

    Update PwdResetToken.js

--- server/Model/PwdResetToken.js
+++ server/Model/PwdResetToken.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 const PwdResetTokenSchema = new mongoose.Schema({
+    token: { type: String, required: true, unique: true},
+    expire_at: { type: Date, required: true },
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+});
 
+PwdResetTokenSchema.pre('save', async function (next) {
+    if (!this.expire_at) {
+        this.expire_at = new Date(Date.now() + 15 * 60 * 1000);
+    }
+    next();
 });
 
 const PwdResetToken = mongoose.model('PwdResetToken', PwdResetTokenSchema);
-
 module.exports = PwdResetToken;
