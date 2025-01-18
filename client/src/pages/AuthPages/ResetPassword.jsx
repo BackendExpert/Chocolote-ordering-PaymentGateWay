3a279200d2e7052843092a425930b0a809f82087
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-18 22:16:16 +0530

    Update ResetPassword.jsx

--- client/src/pages/AuthPages/ResetPassword.jsx
+++ client/src/pages/AuthPages/ResetPassword.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ResetPassword = () => {
+    const {token} = useParams()
   return (
-    <div>ResetPassword</div>
+    <div>{token}</div>
   )
 }
 
