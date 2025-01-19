718175cbcfe5a7c95ca79b5bfe707423b2af4244
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-19 08:38:49 +0530

    Update ResetPassword.jsx

--- client/src/pages/AuthPages/ResetPassword.jsx
+++ client/src/pages/AuthPages/ResetPassword.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const ResetPassword = () => {
+    const navigate = useNavigate()
     const {token} = useParams()
+    const [updatepass, setupdatepass] = useState({
+        newpass: '',
+        confarmpass: '',
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setlogindata((prevData) => ({
+          ...prevData,
+          [name]: value
+        }));
+    };
+
+    const headleupdatepass = async (e) => {
+        e.preventDefault()
+        try{
+            const res = await axios.post(import.meta.env.VITE_APP_API + '/auth/updatepass', updatepass)
+            .then(res => {
+                if(res.data.Status === "Success"){
+                    alert("Password Updated Successful")
+                    navigate('/signin')
+                }
+                else{
+                    alert(res.data.Error)
+                }
+            })
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
   return (
-    <div>{token}</div>
+    <div></div>
   )
 }
 
